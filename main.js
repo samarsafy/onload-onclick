fbfd19d58f92e3870ecdc14e1dc9bcf6cac869d0
Samar <samarsafy@yahoo.de>
2018-03-30 22:06:26 +0200

    onclick-ondblclick-onload

---
+++ main.js
@@ -0,0 +1,8 @@
+/*window.onload = function () {
+  document.getElementById("test").innerHTML = "Text from JavaScript";
+}
+window.onclick = function () {
+  document.getElementById("test").innerHTML = "Text from JavaScript";
+}
+window.ondblclick = function () {
+    document.getElementById("test").innerHTML = "Text from JavaScript"; */
