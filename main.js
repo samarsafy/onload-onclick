8544901fb2a7671aebea53582c80a56777a3e9c2
Samar <samarsafy@yahoo.de>
2018-03-30 22:41:54 +0200

    onkeyup-onkeydown-onkeypress

--- main.js
+++ main.js
@@ -6,3 +6,17 @@ window.onclick = function () {
 }
 window.ondblclick = function () {
     document.getElementById("test").innerHTML = "Text from JavaScript"; */
+
+    var myInput = document.getElementById("input"),
+
+    myDiv = document.getElementById("div");
+   // myButton = document.getElementById("button");
+    // Element.Event = function (){//code}
+   //myButton.onclick = function (){
+     myInput.onkeyup = function () {
+       //myInput.onkeydown = function () {
+         //myInput.onkeypress = function () {
+      "use strict";
+      myDiv. innerHTML = myInput.value * 0.81;
+      //alert("you pressed the button");
+    };
